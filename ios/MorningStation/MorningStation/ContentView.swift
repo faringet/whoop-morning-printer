66f0d84963cd3ab753b38340550ac52b57783460
Mikhail Davydov <mik.davydov14@gmail.com>
2026-06-22 22:21:29 +0300

    feat: Morning Station app shell added

---
+++ ios/MorningStation/MorningStation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MorningStation
+//
+//  Created by Михаил Давыдов on 21.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
